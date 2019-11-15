0ae8db0357242f19f143d5cd1f25e66af01a91f1
xiaolu289 <312351283@qq.com>
2019-11-15 15:31:02 +0800

    修改

--- cmds/server.js
+++ cmds/server.js
@@ -1,6 +1,5 @@
-const cleanArgs = require('../lib/cleanArgs')
-const slash = require('slash')
 const minimist = require('minimist')
+const net = require('net');
 const path = require('path');
 
 module.exports = function(program) {
@@ -11,10 +10,26 @@ module.exports = function(program) {
     .option('-P, --port', 'The port of server')
     .action(()=>{
         var params = minimist(process.argv.slice(3));
-        console.log('params：', params);
-        console.log('cwd：', process.cwd());
-        console.log('__dirname：',__dirname);
-        console.log('./:',path.resolve('./'));
+        var host = params.H;
+        var post = params.P;
+        console.log('host：', host);
+        console.log('post：', post);
+
+        const server = net.createServer((c) => {
+            // 'connection' 监听器。
+            console.log('客户端已连接');
+            c.on('end', () => {
+                console.log('客户端已断开连接');
+            });
+            c.write('你好\r\n');
+            c.pipe(c);
+        });
+        server.on('error', (err) => {
+            throw err;
+        });
+        server.listen(post, () => {
+            console.log('服务器已启动');
+        });
     })
     return program
 }
